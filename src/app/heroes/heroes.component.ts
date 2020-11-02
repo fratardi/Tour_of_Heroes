001a4fa41338796ee60b275d506a00fb7b0bd623
fratardi <fratardi@student.42.fr>
2020-11-02 12:50:06 +0100

    2 files modifiedAdded selector for heroes

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -10,6 +10,13 @@ import { HEROES } from "../mock-heroes"; //// calls the list of mnock heroes
   styleUrls: ["./heroes.component.css"]
 })
 export class HeroesComponent implements OnInit {
+  heroes = HEROES;
+
+  selectedHero: Hero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+
   hero: Hero = {
     id: 1,
     name: "Windstorm"
