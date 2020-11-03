7a7a3036a665955d693099f29f0bf7b88249410d
fratardi <fratardi@student.42.fr>
2020-11-03 16:02:50 +0100

    3 files modified, 2 added Working Routing Module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeroesComponent } from './heroes/heroes.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'detail/:id', component: HeroDetailComponent },
+  { path: 'heroes', component: HeroesComponent }
+];
 
 @NgModule({
-  imports: [
-    CommonModule
-  ],
-  declarations: []
+  imports: [ RouterModule.forRoot(routes) ],
+  exports: [ RouterModule ]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,18 +7,27 @@ import { FormsModule } from "@angular/forms"; //// Ng module referencer
 import { AppComponent } from "./app.component";
 
 import { HeroesComponent } from "./heroes/heroes.component";
-import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { HeroDetailComponent } from "./hero-detail/hero-detail.component";
 
-import { MessagesComponent } from './messages/messages.component';
+import { MessagesComponent } from "./messages/messages.component";
+import { DashboardComponent } from "./dashboard/dashboard.component";
+import { AppRoutingModule } from "./app-routing.module";
+
 //import { HeroService } from './hero.service';
 //import { MessageService } from './message.service';
 //import { HelloComponent } from "./hello.component";
 /// references all the modules present in the app
 
 @NgModule({
-  imports: [BrowserModule, FormsModule],
-  declarations: [AppComponent, HeroesComponent, HeroDetailComponent, MessagesComponent],
-  bootstrap: [AppComponent],
-//  providers: [HeroService, MessageService]
+  imports: [AppRoutingModule, BrowserModule, FormsModule],
+  declarations: [
+    AppComponent,
+    HeroesComponent,
+    HeroDetailComponent,
+    MessagesComponent,
+    DashboardComponent
+  ],
+  bootstrap: [AppComponent]
+  //  providers: [HeroService, MessageService]
 })
 export class AppModule {} /// wraps all the reference for further usage
