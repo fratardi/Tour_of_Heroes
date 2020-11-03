fbee332f1234debad5adeeb864f2da218cc557da
fratardi <fratardi@student.42.fr>
2020-11-03 17:29:08 +0100

    6 files modified EDITED SERVICE

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -16,4 +16,10 @@ export class HeroService {
   constructor(private messageService: MessageService)
    {}
 
+  getHero(id: number): Observable<Hero> {
+    // TODO: send the message _after_ fetching the hero
+    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    return of(HEROES.find(hero => hero.id === id));
+  }
+
 }
