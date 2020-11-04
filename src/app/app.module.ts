215c0be41138529ba23d5bb21f5cecf0bae180e5
fratardi <fratardi@student.42.fr>
2020-11-04 14:45:51 +0100

    2 files modified, 1 added Successfully added import module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,21 +14,32 @@ import { DashboardComponent } from "./dashboard/dashboard.component";
 
 import { AppRoutingModule } from "./app-routing.module";
 
+
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data.service';
+
 //import { HeroService } from './hero.service';
 //import { MessageService } from './message.service';
 //import { HelloComponent } from "./hello.component";
 /// references all the modules present in the app
 
 @NgModule({
-  imports: [AppRoutingModule, BrowserModule, FormsModule],
+  imports: [
+  AppRoutingModule,
+  BrowserModule,
+  FormsModule,
+  HttpClientModule,
+  HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService, { dataEncapsulation: false })],
   declarations: [
     AppComponent,
     HeroesComponent,
     HeroDetailComponent,
     MessagesComponent,
-    DashboardComponent
+    DashboardComponent,
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  providers: [InMemoryDataService]
   //  providers: [HeroService, MessageService]
 })
 export class AppModule {} /// wraps all the reference for further usage
