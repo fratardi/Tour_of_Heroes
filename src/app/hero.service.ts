17dc1d5b3d9affcc82dc006f1513c48f694b7bfe
fratardi <fratardi@student.42.fr>
2020-11-05 10:03:07 +0100

    2 files modified Added Error Handling

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -3,6 +3,8 @@ import { Injectable } from "@angular/core";
 import { Hero } from "./hero";
 import { HEROES } from "./mock-heroes";
 import { Observable, of } from 'rxjs';
+import { catchError, map, tap } from 'rxjs/operators';
+
 import { MessageService } from './message.service';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 
@@ -10,11 +12,31 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 
 export class HeroService {
- 
+
+
+  private handleError<T>(operation = 'operation', result?: T) {
+  return (error: any): Observable<T> => {
+
+    // TODO: send the error to remote logging infrastructure
+    console.error(error); // log to console instead
+
+    // TODO: better job of transforming error for user consumption
+    this.log(`${operation} failed: ${error.message}`);
+
+    // Let the app keep running by returning an empty result.
+    return of(result as T);
+  };
+}
+
+
+
+
+
     getHeroes():  Observable<Hero[]> {
-  // TODO: send the message _after_ fetching the heroes
-  //this.messageService.add('HeroService: fetched heroes');
-  return this.http.get<Hero[]>(this.heroesUrl);
+  return this.http.get<Hero[]>(this.heroesUrl)
+    .pipe(
+      catchError(this.handleError<Hero[]>('getHeroes', []))
+    );
   }
 
 
--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -1,6 +1,6 @@
 import { Hero } from './hero';
 
-export const HEROES: Hero[] = [
+ export const HEROES: Hero[] = [
   { id: 11, name: 'Dr Nice' },
   { id: 12, name: 'Narco' },
   { id: 13, name: 'Bombasto' },
