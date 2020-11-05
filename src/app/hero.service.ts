9225f9c92f56f1ea9685d4d2a367e47e0d357c4a
fratardi <fratardi@student.42.fr>
2020-11-05 10:25:31 +0100

    3 files modified Add heroes feature

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -32,7 +32,12 @@ export class HeroService {
 };
 
 
-
+addHero(hero: Hero): Observable<Hero> {
+  return this.http.post<Hero>(this.heroesUrl, hero, this.httpOptions).pipe(
+    tap((newHero: Hero) => this.log(`added hero w/ id=${newHero.id}`)),
+    catchError(this.handleError<Hero>('addHero'))
+  );
+}
 
     getHeroes():  Observable<Hero[]> {
   return this.http.get<Hero[]>(this.heroesUrl)
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -26,6 +26,16 @@ export class HeroesComponent implements OnInit {
   //  this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
   //}
 
+  add(name: string): void {
+  name = name.trim();
+  if (!name) { return; }
+  this.heroService.addHero({ name } as Hero)
+    .subscribe(hero => {
+      this.heroes.push(hero);
+    });
+}
+
+
   getHeroes(): void {
     this.heroService.getHeroes().subscribe(heroes => (this.heroes = heroes));
   }
